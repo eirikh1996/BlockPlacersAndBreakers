67e00ec3002beac77cecf9c6670405b33c26b163
eirikh1996 <eirikhetland1996@gmail.com>
2019-08-31 00:22:43 +0200

    Added subcommands for /blockplacersandbreakers command

--- src/main/java/io/github/eirikh1996/blockplacersandbreakers/MainCommand.java
+++ src/main/java/io/github/eirikh1996/blockplacersandbreakers/MainCommand.java
@@ -10,20 +10,30 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+
 import static io.github.eirikh1996.blockplacersandbreakers.Messages.BPB_PREFIX;
 import static io.github.eirikh1996.blockplacersandbreakers.Messages.ERROR;
 
 public class MainCommand implements CommandExecutor {
     private final BlockPlacersAndBreakers bpb;
+    private ArrayList<String> subCommands = new ArrayList<>();
 
     public MainCommand(BlockPlacersAndBreakers bpb) {
         this.bpb = bpb;
+        subCommands.add("help");
+        subCommands.add("info");
+        subCommands.add("placers");
+        subCommands.add("breakers");
     }
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (!command.getName().equalsIgnoreCase("blockplacersandbreakers")){
             return true;
+        }
+        else if (strings.length == 0){
+            return false;
         }
         else if (strings[0].equalsIgnoreCase("info")){
             commandSender.sendMessage(ChatColor.DARK_AQUA + "==============================================================");
@@ -34,6 +44,10 @@ public class MainCommand implements CommandExecutor {
             commandSender.sendMessage(ChatColor.DARK_AQUA + "==============================================================");
             return true;
         } else if (strings[0].equalsIgnoreCase("placers")){
+            if (!(commandSender instanceof Player)){
+                commandSender.sendMessage(BPB_PREFIX + ERROR + "You must be player to use the " + strings[0] + " subcommand");
+                return true;
+            }
             if (strings.length == 2 && !commandSender.hasPermission("blockplacersandbreakers.command.placers.other") && !commandSender.hasPermission("bpb.command.placers.other")){
                 commandSender.sendMessage(BPB_PREFIX + ERROR + "You have no permission to check other's block placers");
                 return true;
@@ -47,6 +61,10 @@ public class MainCommand implements CommandExecutor {
                 }
                 return true;
             } else {
+                if (!commandSender.hasPermission("blockplacersandbreakers.command.placers") || !commandSender.hasPermission("bpb.command.placers")){
+                    commandSender.sendMessage(BPB_PREFIX + ERROR + "You have no permission to use the " + strings[0] + "subcommand");
+                    return true;
+                }
                 for (BlockPlacer placer : BlockPlacersAndBreakers.getInstance().getBlockPlacers()){
                     if (!placer.getOwner().equals(((Player) commandSender).getUniqueId())){
                         continue;
@@ -55,6 +73,10 @@ public class MainCommand implements CommandExecutor {
                 }
             }
         } else if (strings[0].equalsIgnoreCase("breakers")){
+            if (!(commandSender instanceof Player)){
+                commandSender.sendMessage(BPB_PREFIX + ERROR + "You must be player to use the " + strings[0] + " subcommand");
+                return true;
+            }
             if (strings.length == 2 && !commandSender.hasPermission("blockplacersandbreakers.command.breakers.other") && !commandSender.hasPermission("bpb.command.breakers.other")){
                 commandSender.sendMessage(BPB_PREFIX + ERROR + "You have no permission to check other's block breakers");
                 return true;
@@ -68,6 +90,10 @@ public class MainCommand implements CommandExecutor {
                 }
                 return true;
             } else {
+                if (!commandSender.hasPermission("blockplacersandbreakers.command.breakers") || !commandSender.hasPermission("bpb.command.breakers")){
+                    commandSender.sendMessage(BPB_PREFIX + ERROR + "You have no permission to use the " + strings[0] + " subcommand");
+                    return true;
+                }
                 for (BlockBreaker breaker : BlockPlacersAndBreakers.getInstance().getBlockBreakers()){
                     if (!breaker.getOwner().equals(((Player) commandSender).getUniqueId())){
                         continue;
@@ -75,8 +101,27 @@ public class MainCommand implements CommandExecutor {
                     commandSender.sendMessage(breaker.getLocation().getWorld().getName() + ": " + breaker.getLocation().toVector().toString());
                 }
             }
+        } else if (strings[0].equalsIgnoreCase("help") || strings[0].equalsIgnoreCase("?")){
+            if (!commandSender.hasPermission("blockplacersandbreakers.command.help") || !commandSender.hasPermission("bpb.command.help")){
+                commandSender.sendMessage(BPB_PREFIX + ERROR + "You have no permission to use the " + strings[0] + " subcommand");
+                return true;
+            }
+            commandSender.sendMessage(ChatColor.DARK_AQUA +"==============={" + ChatColor.GREEN + "BlockPlacersAndBreakers v" + bpb.getDescription().getVersion() + ChatColor.DARK_AQUA + "}===============");
+            commandSender.sendMessage("Use a " + Settings.tool.name().toLowerCase().replace("_", " ") + " as your selection tool to turn dispensers into block placers/breakers");
+            commandSender.sendMessage("To apply/remove block placing abilities, left click with your selection tool");
+            commandSender.sendMessage("To apply/remove block breaking abilities, right click with your selection tool");
+            commandSender.sendMessage("Fill block placers with the blocks to be placed and block breakers with pickaxes");
+            ArrayList<String> allowedSubCmds = new ArrayList<>();
+            for (String subCmd : subCommands){
+                if (!commandSender.hasPermission("blockplacersandbreakers.command." + subCmd) || !commandSender.hasPermission("bpb.command." + subCmd)){
+                    continue;
+                }
+                allowedSubCmds.add(subCmd);
+            }
+            commandSender.sendMessage("Subcommands: " + String.join(", ", allowedSubCmds));
+            commandSender.sendMessage(ChatColor.DARK_AQUA + "=====================================================================");
         }
 
-        return false;
+        return true;
     }
 }
